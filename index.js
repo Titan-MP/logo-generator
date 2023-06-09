babc7eaf271a2692e2e63c34ce2b7d2ac16ca918
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-06-08 22:59:07 -0400

    coded with autopilot base code for hw

---
+++ index.js
@@ -0,0 +1,34 @@
+const {prompt} = require('inquirer');
+
+prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter three letters for logo:',
+        validate: text => text.length == 3 || 'Please enter three letters.', 
+        default: 'ABC'
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What color would you like your text to be:',
+        default: 'white'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['square', 'circle', 'triangle'], 
+        default: 'circle'
+
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color would you like your shape color to be?',
+        default: 'blue'
+    }
+
+]).then((ans) => {
+    console.log(ans);
+} );
