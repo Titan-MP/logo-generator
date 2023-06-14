c2c569966753df068f1057e014e4c14c063849b6
Titan-MP <marcrhymaun@knights.ucf.edu>
2023-06-13 20:59:07 -0400

    fixed errors on logo

--- index.js
+++ index.js
@@ -1,34 +1,4 @@
 const {prompt} = require('inquirer');
+const { questions, svgGen } = require('./lib');
 
-prompt([
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter three letters for logo:',
-        validate: text => text.length == 3 || 'Please enter three letters.', 
-        default: 'ABC'
-    },
-    {
-        type: 'input',
-        name: 'color',
-        message: 'What color would you like your text to be:',
-        default: 'white'
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Choose a shape:',
-        choices: ['square', 'circle', 'triangle'], 
-        default: 'circle'
-
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What color would you like your shape color to be?',
-        default: 'blue'
-    }
-
-]).then((ans) => {
-    console.log(ans);
-} );
+prompt(questions).then(svgGen);
